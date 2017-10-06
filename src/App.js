452d38615dde5a0b004aeb7a8324dee633269dde
Peter Tyldesley <ptyldesley@schawk.com>
2017-10-06 16:51:48 +0100

    Add long press to 'DEL' button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import CalculatorDisplay from './components/calculator_display';
 import CalculatorButton from './components/calculator_button';
+import ClickNHold from 'react-click-n-hold';
 
 class App extends Component {
   constructor(props) {
@@ -9,6 +10,7 @@ class App extends Component {
     this.state = {result: null, calculation: "", currentNum: "", prevButtonType: "", firstNum: null, secondNum: null, operator: ""};
     this.handleInputClick = this.handleInputClick.bind(this);
     this.handleControlClick = this.handleControlClick.bind(this);
+    this.clearScreen = this.clearScreen.bind(this);
   }
 
   handleInputClick(number) {
@@ -25,7 +27,7 @@ class App extends Component {
   handleControlClick(control) {
     switch(control) {
       case 'DEL': {
-        this.setState({calculation: "", result: null});
+        this.setState({calculation: this.state.calculation.slice(0, this.state.calculation.length - 1), result: null});
         break;
       }
       case '+': {
@@ -54,6 +56,10 @@ class App extends Component {
     }
   }
 
+  clearScreen() {
+    this.setState({calculation: "", result: null});
+  }
+
   render() {
     return (
         <div className="App">
@@ -63,11 +69,16 @@ class App extends Component {
             </div>
             <div className="calculator-inputs">
               <div className="column">
-                <CalculatorButton 
-                  value="DEL" 
-                  className="calculator-control" 
-                  onClick={this.handleControlClick}
-                />
+                <ClickNHold
+                  time={0.5}
+                  onClickNHold={this.clearScreen}
+                >
+                  <CalculatorButton 
+                    value="DEL" 
+                    className="calculator-control" 
+                    onClick={this.handleControlClick}
+                  />
+                </ClickNHold>
                 <CalculatorButton 
                   value="&divide;" 
                   className="calculator-control" 
