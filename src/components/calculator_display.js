cf6c41ba8ed1d0b0a8bcd0f52c5f413e074df4a9
Peter Tyldesley <ptyldesley@schawk.com>
2017-10-10 15:28:42 +0100

    Add size variable

--- src/components/calculator_display.js
+++ src/components/calculator_display.js
@@ -4,9 +4,10 @@ class CalculatorDisplay extends Component {
   render() {
     const maxSize = 40;
     const charCount = this.props.charCount / 1.5;
+    const size = maxSize - charCount;
 
     return (
-      <div style={{'fontSize': `${maxSize - charCount}px`}}>
+      <div style={{'fontSize': `${size}px`}}>
         <div className="calculation">{this.props.calculation}</div>
         <div className="result">{this.props.result}</div>
       </div>
